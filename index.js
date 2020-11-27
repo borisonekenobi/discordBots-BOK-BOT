cb1f32281517f1402deca70a94a2f77476426c94
Boris <borisonekenobi@gmail.com>
2020-11-26 22:41:09 -0500

    Gives roles, was bug before

--- index.js
+++ index.js
@@ -132,41 +132,41 @@ function getUserData(options, msg, rolesFile) {
                 userData.push([user.id, user.level]);
             }
 
-            // By Kiril
             msg.channel.send('Updating roles').then(r => console.log(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, "\n\t")}`)).catch(console.error)
                 .then(r => {
-                const contents = fs.readFileSync(rolesFile, 'utf8');
-                const lines = contents.split('\n');
-                const args = [];
-                for (let i = 0; i < lines.length; i++) {
-                    if (lines[i] !== '') {
-                        args.push(lines[i].split(' '))
+                    const contents = fs.readFileSync(rolesFile, 'utf8');
+                    const lines = contents.split('\n');
+                    const args = [];
+                    for (let i = 0; i < lines.length; i++) {
+                        if (lines[i] !== '') {
+                            args.push(lines[i].split(' '));
+                        }
                     }
-                }
-                shuffle(userData);
-                for (let userInfo of userData) {
-                    let memberId = userInfo[0];
-                    if (memberId === undefined) {
-                        continue;
-                    }
-                    let member = msg.guild.members.cache.get(memberId)
-                    for (let arg of args) {
-                        let roleLevel = Number(arg[1]);
-                        let roleId = arg[0];
-                        let role = msg.guild.roles.cache.find(role => role.id === roleId);
-                        if (roleLevel === 0) {
-                            if (member !== undefined) {
-                                member.roles.add(role);
-                            }
-                        } else if (Number(userInfo[1]) >= roleLevel) {
-                            if (member !== undefined) {
-                                member.roles.add(role);
+                    for (let userInfo of userData) {
+                        let memberId = userInfo[0];
+                        if (memberId === undefined) {
+                            continue;
+                        }
+                        let member = msg.guild.members.cache.get(memberId);
+                        for (let arg of args) {
+                            let roleLevel = Number(arg[1]);
+                            let roleId = arg[0];
+                            let role = msg.guild.roles.cache.find(role => role.id === roleId);
+                            if (roleLevel === 0) {
+                                if (member !== undefined) {
+                                    member.roles.add(role);
+                                }
+                            } else if (Number(userInfo[1]) >= roleLevel) {
+                                if (member !== undefined) {
+                                    member.roles.add(role);
+                                }
                             }
                         }
                     }
-                }
+                })
+                .then(r => {
+                    msg.channel.send('Done setup. Use !bok help for help').then(r => console.log(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, "\n\t")}`)).catch(console.error);
                 });
-            msg.channel.send('Done setup. Use !bok help for help').then(r => console.log(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, "\n\t")}`)).catch(console.error);
         })
         .catch((err) => {
             console.error(err);
@@ -201,23 +201,4 @@ function checkID(IDnum, path) {
     for (let i = 0; i < IDs.length; i++) {
         return IDnum === IDs[i];
     }
-}
-
-function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
 }
