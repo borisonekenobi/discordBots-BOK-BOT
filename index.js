6e667fac1cdc08be9de12b82a6546e015263c389
Boris <borisonekenobi@gmail.com>
2020-11-27 10:27:14 -0500

    Fixed bug --> Was sending entire role instead of role.id

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ bot.on('message', msg => {
                 if (Number(msgArgs[7]) === args[i][1]) {
                     role = member.guild.roles.find(role => role.id === args[i][0]);
                     member.roles.add(role).then(console.log("Role " + role.id + " given to " + member.id)).catch(console.error);
-                    msg.channel.send('Congratulations <@' + member.id + '>, you have just received the <@&' + role + '> role!').then(r => console.log(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, "\n\t")}`)).catch(console.error);
+                    msg.channel.send('Congratulations <@' + member.id + '>, you have just received the <@&' + role.id + '> role!').then(r => console.log(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, "\n\t")}`)).catch(console.error);
                     break;
                 }
             }
