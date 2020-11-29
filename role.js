5f4c01f58f5c89261248fbb8fe9564350c79dc4b
Boris <borisonekenobi@gmail.com>
2020-11-29 12:10:43 -0500

    Added roleEdit and roleList calls to role.js

--- role.js
+++ role.js
@@ -1,20 +1,42 @@
+const util = require('./util.js');
 const ra = require('./roleAdd.js');
+const re = require('./roleEdit.js');
+const rl = require('./roleList.js');
 const rr = require('./roleRemove.js');
 
 function role(msg, msgContent, rolesFile) {
-        // Adds role to roleFile
-    if (msgContent.startsWith('!bok role add')) {
-        ra.roleAdd(msg, msgContent, rolesFile);
+    if (util.isAdmin(msg)) {
+        const args = msgContent.slice(0).split(' ');
+        // Checks if user has inputted function
+        if (args [2] === undefined) {
+            msg.channel.send('That is an invalid role! Please mention a role.')
+                .then(r => console.warn(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, '\n\t')}`)).catch(console.error);
 
-        // Removes role from roleFile
-    } else if (msgContent.startsWith('!bok role remove')) {
-        rr.roleRemove(msg, msgContent, rolesFile);
+            // Adds role to roleFile
+        } else if (msgContent.startsWith('!bok role add')) {
+            ra.roleAdd(msg, msgContent, rolesFile);
 
-        //
-    } /*else {
+            // Edits role in roleFile
+        } else if (msgContent.startsWith('!bok role edit')) {
+            re.roleEdit(msg, msgContent, rolesFile);
+
+            // Lists all setup roles
+        } else if (msgContent.startsWith('!bok role list')) {
+            rl.roleList(msg, msgContent, rolesFile);
+
+            // Removes role from roleFile
+        } else if (msgContent.startsWith('!bok role remove')) {
+            rr.roleRemove(msg, msgContent, rolesFile);
+
+            //
+        } /*else {
         msg.channel.send('That is an invalid argument, please specify whether you are adding or removing role!')
             .then(r => console.error(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, '\n\t')}`)).catch(console.error);
     }*/
+    } else {
+        msg.channel.send('You do not have admin permissions!')
+            .then(r => console.warn(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, '\n\t')}`)).catch(console.error);
+    }
 }
 
 module.exports = {role}
