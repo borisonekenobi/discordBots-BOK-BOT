f2d2194e462b0e4f4eff9f54987cc81c225a491c
Boris <borisonekenobi@gmail.com>
2020-11-28 19:10:42 -0500

    Fixed bug --> crashed if no role was mentioned

--- roleAdd.js
+++ roleAdd.js
@@ -8,30 +8,30 @@ function roleAdd(msg, msgContent, rolesFile) {
         let roleLevel = parseFloat(args[4]);
         let role = msg.mentions.roles.first();
 
-            // Checks where role is mentioned
-        if (args[3] !== ('<@&' + role.id + '>')) {
+            // Checks that a role is mentioned
+        if (typeof role !== 'object') {
             msg.channel.send('That is an invalid role! Please mention a role.')
                 .then(r => console.warn(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, '\n\t')}`)).catch(console.error);
 
-            // Checks that a role is mentioned
-        } else if (typeof role !== 'object') {
+            // Checks where role is mentioned
+        } else if (args[3] !== ('<@&' + role.id + '>')) {
             msg.channel.send('That is an invalid role! Please mention a role.')
                 .then(r => console.warn(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, '\n\t')}`)).catch(console.error);
 
             // Checks that roleLevel is a number
         } else if (typeof roleLevel != 'number' || isNaN(roleLevel)) {
             msg.channel.send('That is an invalid role level! Please enter a number.')
-                .then(r => console.log(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, '\n\t')}`)).catch(console.error);
+                .then(r => console.warn(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, '\n\t')}`)).catch(console.error);
 
             // Checks that there are no extra arguments
         } else if (args[5] !== undefined) {
             msg.channel.send('Too many arguments provided!')
-                .then(r => console.log(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, '\n\t')}`)).catch(console.error);
+                .then(r => console.warn(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, '\n\t')}`)).catch(console.error);
 
             // Checks if role already exists in rolesFile
         } else if (util.checkID(role.id, rolesFile)) {
             msg.channel.send('That role is already setup!')
-                .then(r => console.log(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, '\n\t')}`)).catch(console.error);
+                .then(r => console.warn(`Sent message: \n\t${r.content.replace(/\r?\n|\r/g, '\n\t')}`)).catch(console.error);
 
             // Adds role to file
         } else {
