2cdcaa9af78d794c4c55443575748c44e5e8fcef
Boris <borisonekenobi@gmail.com>
2021-01-04 22:03:18 -0500

    Added try-catch blocks to functions

--- newBotMember.js
+++ newBotMember.js
@@ -1,12 +1,16 @@
 const util = require('./util.js');
 
 function newBotMember(member, botRolesFile) {
-    const args = util.getFileData(botRolesFile);
-    for (let i = 0; i < args.length - 1; i++) {
-        let roleId = args[i][0].trim();
-        roleId.toString();
-        let role = member.guild.roles.cache.find(r => r.id === roleId);
-        util.giveRole(member, role, roleId)
+    try {
+        const args = util.getFileData(botRolesFile);
+        for (let i = 0; i < args.length - 1; i++) {
+            let roleId = args[i][0].trim();
+            roleId.toString();
+            let role = member.guild.roles.cache.find(r => r.id === roleId);
+            util.giveRole(member, role, roleId)
+        }
+    } catch (err) {
+        util.createLog(err);
     }
 }
 
