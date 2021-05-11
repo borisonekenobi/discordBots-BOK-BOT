222a019f0aec8ed8a6b5a89e4802b09d54828cf4
Boris <borisonekenobi@gmail.com>
2021-05-11 10:54:32 -0400

    Added a way to setup logs

--- commands/serverLogs/setup.js
+++ commands/serverLogs/setup.js
@@ -3,7 +3,7 @@ const util = require('../../util.js');
 const fs = require("fs");
 
 function setup(interaction, guild) {
-    return 'currently being worked on';
+    //return 'currently being worked on';
     let channelID = Object.keys(interaction.data.resolved.channels)[0]
     let logsFile = util.createFile('./servers/' + guild.id + '.logsfile');
     fs.writeFileSync(logsFile, channelID);
