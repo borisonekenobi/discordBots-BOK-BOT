9268b285c2386c3887a40decb6ada3d78bdcdb58
Boris <borisonekenobi@gmail.com>
2021-05-11 10:53:48 -0400

    Added a way to disable logs

--- commands/serverLogs/disable.js
+++ commands/serverLogs/disable.js
@@ -1,7 +1,21 @@
 const util = require('../../util.js');
 
+const fs = require("fs");
+
 function disable(interaction, guild) {
-    return 'currently being worked on';
+    //return 'currently being worked on';
+    const file = './servers/' + guild.id + '.logsfile';
+    util.createFile(file);
+    const data = fs.readFileSync(file, 'utf8');
+
+    if (data === '') {
+        return 'No logs channel is setup yet!'
+
+    } else {
+        fs.writeFileSync(file, '');
+
+        return 'The logs channel has been removed. Logs will no longer be kept'
+    }
 }
 
 module.exports = {disable}
