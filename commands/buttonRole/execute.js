9d1012a48b9b000f94046cde3916a89e9237783c
Boris <borisonekenobi@gmail.com>
2021-08-24 22:52:41 -0400

    Added ability to remove roles by re-clicking button

--- commands/buttonRole/execute.js
+++ commands/buttonRole/execute.js
@@ -21,7 +21,11 @@ function execute(interaction, rolesFile, guild) {
 function buttonClicked(interaction, author, guild) {
     let roleID = interaction.data.custom_id;
     let role = guild.roles.cache.find(role => role.id === roleID);
-    util.giveRole(author, role, roleID);
+    if (util.hasRole(author, roleID)) {
+        util.removeRole(author, role);
+    } else {
+        util.giveRole(author, role, roleID);
+    }
 }
 
 module.exports = {execute, buttonClicked}
