61d355d34eccf50dbb883f5d65d4a717e46106d3
Zurel <90565581+Zur3l@users.noreply.github.com>
2024-05-30 22:50:51 -0400

    Update level.js

--- commands/level.js
+++ commands/level.js
@@ -1,5 +1,6 @@
 const util = require("../util");
 const dbLevel = require("../db/dbLevel");
+var Jimp = require("jimp");
 
 async function execute(guildID, member, otherMember = false) {
     const userID = member.id;
@@ -14,19 +15,37 @@ async function execute(guildID, member, otherMember = false) {
     const numFilled = Math.round(percent / 5);
     const numEmpty = 20 - numFilled;
 
-    const underline = '⠀'.repeat(10);
+    var image;
 
-    const filled = '█'.repeat(numFilled);
-    const empty = '▒'.repeat(numEmpty);
+    Jimp.read("https://cdn.discordapp.com/attachments/546366129673863170/1245254758021730304/card.png?ex=665a0f9e&is=6658be1e&hm=d3d56744f0b34b268986751a3ce0624ddbb9656cf5eb5344743f19a60e3ad792&", (err, img) => {
+      if (err) throw err;
+          Jimp.read(member.displayAvatarURL(), (err, pfp) => {
+          if (err) throw err;
+          img.composite(pfp, 30, 30); //i have no clue what numbers to use i dont care that hard about this
+        });
 
-    const fields = [
-        {
-            name: `__${underline}⠀${percent}%${underline}${percent >= 10 ? '' : '⠀'}__`,
-            value: `${user.level} ${filled}${empty} ${user.level + 1}`
-        }
-    ]
+      Jimp.loadFont(pathOrURL).then((font) => { //todo add a font
+          image.print(font, 60, 60, user.name);
+          image.print(font, 60, 90, "Level: " + user.level);
+      });
+      image = img;
+    });
 
-    return util.createEmbed('#00FF00', `${otherMemberText}Rank #${user.rank} - Level ${user.level}`, '', '', '', '', `${user.points}/${pointsForNextLevel}`, '', fields);
+    return image;
+
+    // const underline = '⠀'.repeat(10);
+
+    // const filled = '█'.repeat(numFilled);
+    // const empty = '▒'.repeat(numEmpty);
+
+    // const fields = [
+    //     {
+    //         name: `__${underline}⠀${percent}%${underline}${percent >= 10 ? '' : '⠀'}__`,
+    //         value: `${user.level} ${filled}${empty} ${user.level + 1}`
+    //     }
+    // ]
+
+    // return util.createEmbed('#00FF00', `${otherMemberText}Rank #${user.rank} - Level ${user.level}`, '', '', '', '', `${user.points}/${pointsForNextLevel}`, '', fields);
 }
 
 function infinite() {
